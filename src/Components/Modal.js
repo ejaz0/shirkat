e28a8f929688ef40f6a23f34c7ffff7a93b9b5bd
reeshy <64738767+reeshy@users.noreply.github.com>
2024-06-24 16:49:20 +0200

    Modal Translation

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import '../Styles/Css/Modal.css';
 
 const Modal = ({ isOpen, onClose }) => {
+    const { t } = useTranslation();
+
     return (
         <div className={`modal ${isOpen ? 'open' : ''}`}>
             <div className="modal-content">
-                <h2>Thanks for your message!</h2>
-                <p>We will contact you soon.</p>
-                <button className="close-button" onClick={onClose}>Close</button>
+                <h2>{t('modal.thanks_title')}</h2>
+                <p>{t('modal.thanks_message')}</p>
+                <button className="close-button" onClick={onClose}>{t('modal.close_button')}</button>
             </div>
         </div>
     );
